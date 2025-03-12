cc1bb01003b38973bfd130c5d5169f8f335ec80f
JordiVU <148686789+JordiVU@users.noreply.github.com>
2025-03-12 19:11:37 +0100

    Add files via upload

---
+++ Ejercicio_08a_05/Ejercicio_08a_05/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<string> texto = new List<string>();
+        string busca;
+
+        using (StreamReader fichero = new StreamReader("diccionario.txt"))
+        {
+            string linea;
+
+            do
+            {
+                linea = fichero.ReadLine();
+                if (linea != null)
+                {
+                    texto.Add(linea);
+                }
+            }
+            while (linea != null);
+        }
+
+        do
+        {
+            Console.Write("Texto a buscar: ");
+            busca = Console.ReadLine();
+
+            for (int i = 0; i < texto.Count(); i++)
+            {
+                if (texto[i].Contains(busca))
+                {
+                    Console.WriteLine(i + ". " + texto[i]);
+                }
+            }
+        }
+        while (busca != "");
+
+
+    }
+}
