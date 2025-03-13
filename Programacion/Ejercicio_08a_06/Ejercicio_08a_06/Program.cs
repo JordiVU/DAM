e605373d7ad68ce116f3cafb6e7752953abd9e28
JordiVU <jordivazquezuribe@gmail.com>
2025-03-13 15:30:28 +0100

    Mover

---
+++ Programacion/Ejercicio_08a_06/Ejercicio_08a_06/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        SortedSet<string> texto = new SortedSet<string>();
+
+        using (StreamReader fichero = new StreamReader("diccionario.txt"))
+        {
+            string linea;
+
+            do
+            {
+                linea = fichero.ReadLine();
+                if (linea != null)
+                {
+                    texto.Add(linea);
+                }
+            }
+            while (linea != null);
+        }
+
+        using (StreamWriter ordenado = new StreamWriter("ordenado.txt"))
+        {
+            
+            foreach(string t in texto)
+            {
+                ordenado.WriteLine(t);
+            }
+        }
+    }
+}
